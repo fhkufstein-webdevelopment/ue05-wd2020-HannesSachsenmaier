ed9ad0caa5d6fd8d7fbe6ace80de2a47b18cbf66
Hannes Sachsenmaier <1910653347@stud.fh-kufstein.ac.at>
2020-03-19 21:18:44 +0100

    Adding rows implemented, delete function not accomplished

--- js/user.js
+++ js/user.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     var userListBody = $('.userList tbody');
 
     //@todo store and somehow update the current number of users
-
+    var tableBody = $("#tableBody");
+    var count = (tableBody.find('tr').length)+1; // Count the tr tags within the tableBody tag
 
     $('.needs-validation').submit(function(event) {
 
@@ -19,13 +20,23 @@ $(document).ready(function() {
 
         //@todo
         //1. get values
+        var username = $("#username").val();
+        var deleteButton = '<button id="deleteButton" type="button" class="btn btn-secondary btn-danger deleteTrigger" title="Löschen"><i class="fa fa-trash"></i></button>'; //Html Tag Button
+
         //2. create a new element
+
+
         //3. somehow add them to userListBody
+        tableBody.append("<tr><td>" + count + "</td><td>"+ username + "</td><td>" + deleteButton + "</td></tr>");
+
         //4. update number of current users
+        count++;
+
         //5. clear entries from the form
+        $("#username").val(" ");
         //6. maybe do something else... :-)
 
-        //your code follows here
+
 
         return false;
     });
@@ -34,6 +45,7 @@ $(document).ready(function() {
     $('.deleteTrigger').click(function() {
         //@todo
         //1. remove current user from dom
+        $(this).parentNode.parentNode.remove(); //Delete function doesnt work. I havent found a way to get to the correct element
         //2. update number of current users
 
         //your code follows here
